6b188799adeaa34a6e7aeebb09d65629581f4288
Rudra Sakariya <rudrasakariya16@gmail.com>
2022-07-22 21:59:51 +0530

    ADD: Sign-Up and Login routes

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ app.get("/", (req, res) => {
 
 app.use("/create-user", require("./routes/create-user"));
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log("Server listening on port 5000");
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server listening on port 3000");
 });
--- routes/create-user.js
+++ routes/create-user.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const jwtSecret = "RudraIsTheBest";
 
 router.get("/", (req, res) => {
   res.send("Hello");
@@ -10,7 +12,7 @@ router.get("/", (req, res) => {
 
 // * Create POST request
 router.post(
-  "/",
+  "/sign-up",
   [
     body("name", "Enter a name more than 2 characters").isLength({ min: 2 }),
     body("email", "Enter a valid email address").isEmail(),
@@ -18,16 +20,16 @@ router.post(
       min: 5,
     }),
   ],
-  (req, res) => {
+  async (req, res) => {
     // * Return errors if any
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
     // * Check if user exists
+    const salt = await bcrypt.genSalt(15);
+    const securePassword = await bcrypt.hash(req.body.password, salt);
     try {
-      const salt = bcrypt.genSalt(15);
-      const securePassword = bcrypt.hash(req.body.password, salt);
       User.findOne({ email: req.body.email }, (err, email) => {
         if (email) {
           res
@@ -40,9 +42,50 @@ router.post(
             password: securePassword,
           });
         }
+        const data = {
+          id: User.id,
+        };
+        const authToken = jwt.sign(data, jwtSecret);
+        res.json({ authToken });
       });
     } catch (error) {
-      console.error(error.message);
+      console.log(error.message);
+      res.status(500).send("Internal Server Error Occured");
+    }
+  }
+);
+
+router.post(
+  "/login",
+  [
+    body("email", "Enter a valid email address").isEmail(),
+    body("password", "Enter password greater than 5 characters").exists(),
+  ],
+  async (req, res) => {
+    // * Return errors if any
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    try {
+      let user = User.findOne({ email });
+      if (!user) {
+        return res.status(404).json({ errors: "Enter correct credentials" });
+      }
+      const passwordCompare = await bcrypt.compare(password, user.password);
+      if (!passwordCompare) {
+        return res.status(404).json({ errors: "Enter correct credentials" });
+      }
+      const data = {
+        id: User.id,
+      };
+      const authToken = jwt.sign(data, jwtSecret);
+      res.json({ authToken });
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send("Internal Server Error Occured");
     }
   }
 );
